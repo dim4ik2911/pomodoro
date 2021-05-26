bfb428e0e3ae5ad81207ba90641563fb4b85f178
Dmitrijs Paklons <dim4ik2911@gmail.com>
2021-05-26 20:16:20 +0100

    progress achieved

--- main.js
+++ main.js
@@ -1,45 +1,54 @@
-const timer = {
-  pomodoro: 25,
-  shortBreak: 5,
-  longBreak: 15,
-  longBreakInterval: 4,
+const pomodoroButton = document.getElementById("pomodoro");
+const breakButton = document.getElementById("break");
+const startButton = document.getElementById("start");
+const time = document.getElementById("timer");
+const audio = document.getElementById("audio");
+const playButton = document.getElementById("play-button");
+
+time.innerHTML = `25 00`;
+
+function startTimer() {
+  let presentTime = time.innerHTML;
+  let timeArray = presentTime.split(" ");
+  let m = parseInt(timeArray[0]);
+  let s = checkSeconds(parseInt(timeArray[1] - 1));
+  if (s == 59) {
+    m = m - 1;
+  }
+  if (m < 10) {
+    m = "0" + m;
+  }
+  time.innerHTML = `${m} ${s}`;
+}
+
+function checkSeconds(sec) {
+  if (sec < 10 && sec >= 0) {
+    sec = "0" + sec;
+  }
+  if (sec < 0) {
+    sec = "59";
+  }
+  return sec;
+}
+
+startButton.addEventListener("click", () => {
+  setInterval(startTimer, 1000);
+  startButton.disabled = true;
+});
+
+breakButton.addEventListener("click", () => {
+  time.innerHTML = `05 00`;
+  stopTimer(countDown);
+});
+
+pomodoroButton.addEventListener("click", () => {
+  time.innerHTML = `25 00`;
+});
+
+playButton.addEventListener("click", () => {
+  audio.play();
+});
+
+const stopTimer = () => {
+  clearInterval(startTimer);
 };
-
-const modeButtons = document.querySelector("#mode-buttons");
-modeButtons.addEventListener("click", chooseMode);
-
-function updateTime() {
-  const remainingTime = timer.remainingTime;
-  const minutes = `${remainingTime.minutes}`.padStart(2, "0");
-  const seconds = `${remainingTime.seconds}`.padStart(2, "0");
-
-  const min = document.getElementById("time-minutes");
-  const sec = document.getElementById("time-seconds");
-  min.textContent = minutes;
-  sec.textContent = seconds;
-}
-
-function switchMode(mode) {
-  timer.mode = mode;
-  timer.remainingTime = {
-    total: timer[mode] * 60,
-    minutes: timer[mode],
-    seconds: 0,
-  };
-
-  document
-    .querySelectorAll("button[data-mode]")
-    .forEach((e) => e.classList.remove("active"));
-  document.querySelector(`[data-mode="${mode}"]`).classList.add("active");
-  document.body.style.backgroundColor = `var(--${mode})`;
-
-  updateTime();
-}
-
-function chooseMode(event) {
-  const { mode } = event.target.dataset;
-
-  if (!mode) return;
-
-  switchMode(mode);
-}
