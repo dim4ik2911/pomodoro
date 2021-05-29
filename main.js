487aa058cf3b8659712b4cf64ed186cf8750ff49
Dmitrijs Paklons <dim4ik2911@gmail.com>
2021-05-29 15:02:42 +0100

    audio play and pause button

--- main.js
+++ main.js
@@ -2,8 +2,6 @@ const pomodoroButton = document.getElementById("pomodoro");
 const breakButton = document.getElementById("break");
 const startButton = document.getElementById("start");
 const time = document.getElementById("timer");
-const audio = document.getElementById("audio");
-const playButton = document.getElementById("play-button");
 
 time.innerHTML = `00 10`;
 
@@ -34,10 +32,22 @@ function checkSeconds(sec) {
   return sec;
 }
 
+let timeClicked = 2;
 startButton.addEventListener("click", () => {
   let timingInterval = setInterval(startTimer, 1000);
   timingInterval;
   startButton.disabled = true;
+  // timeClicked++;
+  // if (timeClicked % 2 !== 0) {
+  //   console.log(timeClicked);
+  //   timingInterval;
+  //   startButton.innerHTML = `Stop`;
+  // }
+  // if (timeClicked % 2 == 0) {
+  //   console.log(timeClicked);
+  //   clearInterval(timingInterval);
+  //   startButton.innerHTML = `Start`;
+  // }
 
   breakButton.addEventListener("click", () => {
     time.innerHTML = `00 05`;
@@ -62,6 +72,29 @@ pomodoroButton.addEventListener("click", () => {
   clearInterval(startTimer);
 });
 
+//audio
+const playButton = document.getElementById("play-button");
+const audio = document.getElementById("audio");
+let isPlaying = false;
+const audioPlayer = () => {
+  isPlaying ? audio.pause() : audio.play();
+};
+const audioImage = () => {
+  if (isPlaying) {
+    playButton.src = "./img/play.svg";
+  } else {
+    playButton.src = "./img/pause.svg";
+  }
+};
+
+audio.onplaying = () => {
+  isPlaying = true;
+};
+audio.onpause = () => {
+  isPlaying = false;
+};
+
 playButton.addEventListener("click", () => {
-  audio.play();
+  audioPlayer();
+  audioImage();
 });
